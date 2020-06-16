1ccc4fa03149f18ee7ab94f43372b177599cd260
Mafatime Gadiaga <mafatimegadiaga@gmail.com>
2020-06-16 01:31:08 +0100

    admin page

--- scrip.js
+++ scrip.js
@@ -3,7 +3,7 @@ function testLogin (){
     var pass = document.getElementById("password");
 
     if(login.value == "mafatimegadiaga@gmail.com" && pass.value =="passer"){
-        alert("you are login successfully")
+        alert("you are log in successfully")
     }else{
         alert("email ou password incorect")
     }
