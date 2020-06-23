c89cbbd3e05e079242e5fef81577b6c7d2a9a956
Mafatime Gadiaga <mafatimegadiaga@gmail.com>
2020-06-23 15:21:17 +0100

    update

--- compte/compte.js
+++ compte/compte.js
@@ -1,26 +1,133 @@
+
 function addCompte(){
+    if(document.getElementById("numCompte").value.trim()=="" && document.getElementById("idclient").value.trim()==""
+    && document.getElementById("numAgence").value.trim()=="" && document.getElementById("solde").value.trim()==""
+    && document.getElementById("dateO").value.trim()=="" && ((document.getElementById("epargne").checked == false && (document.getElementById("simple").checked == false || document.getElementById("xeweul").checked == false))
+    || document.getElementById("courant").checked == false ||document.getElementById("bloque").checked == false) ){
+       afficheTout()
+    }else if(document.getElementById("numCompte").value.trim()==""){
 
-    var formdata = readForm();
-    insertCompte(formdata)
+        document.getElementById("champs2").style.display= "block";
+ 
+        document.getElementById("champs1").style.display= "none";
+        document.getElementById("champs3").style.display= "none";
+        document.getElementById("champs4").style.display= "none";
+        document.getElementById("champs5").style.display= "none";
+        
+    }else if(document.getElementById("idclient").value.trim()==""){
 
+        document.getElementById("champs1").style.display= "block";
+
+        
+        document.getElementById("champs2").style.display= "none";
+        document.getElementById("champs3").style.display= "none";
+        document.getElementById("champs4").style.display= "none";
+        document.getElementById("champs5").style.display= "none";
+        
+    }else if(document.getElementById("numAgence").value.trim()==""){
+
+        document.getElementById("champs3").style.display= "block";
+
+        
+        document.getElementById("champs1").style.display= "none";
+        document.getElementById("champs2").style.display= "none";
+        document.getElementById("champs4").style.display= "none";
+        document.getElementById("champs5").style.display= "none";
+
+
+    }else if(document.getElementById("solde").value.trim()==""){
+
+    document.getElementById("champs4").style.display= "block";
+    
+    document.getElementById("champs1").style.display= "none";
+    document.getElementById("champs3").style.display= "none";
+    document.getElementById("champs2").style.display= "none";
+    document.getElementById("champs5").style.display= "none";
+
+   }else if(document.getElementById("dateO").value.trim()==""){
+
+    document.getElementById("champs5").style.display= "block";
+
+    
+    document.getElementById("champs1").style.display= "none";
+    document.getElementById("champs3").style.display= "none";
+    document.getElementById("champs4").style.display= "none";
+    document.getElementById("champs2").style.display= "none";
+
+    }else if((document.getElementById("epargne").checked == false && (document.getElementById("simple").checked == false || document.getElementById("xeweul").checked == false))
+    || document.getElementById("courant").checked == false ||document.getElementById("bloque").checked == false  ){
+
+        document.getElementById("champs6").style.display= "block";
+    
+        document.getElementById("champs1").style.display= "none";
+        document.getElementById("champs3").style.display= "none";
+        document.getElementById("champs4").style.display= "none";
+        document.getElementById("champs5").style.display= "none";
+        document.getElementById("champs2").style.display= "none";
+    
+        }else{
+        var formdata = readForm();
+        insertCompte(formdata);
+        annulChamp();
+    }
+    
+
+}
+
+function afficheTout() {
+    
+    document.getElementById("champs5").style.display= "block"; 
+    document.getElementById("champs1").style.display= "block";
+    document.getElementById("champs3").style.display= "block";
+    document.getElementById("champs4").style.display= "block";
+    document.getElementById("champs2").style.display= "block";
+    document.getElementById("champs6").style.display= "block";
+
+}
+function annulTout() {
+    
+    document.getElementById("champs5").style.display= "none"; 
+    document.getElementById("champs1").style.display= "none";
+    document.getElementById("champs3").style.display= "none";
+    document.getElementById("champs4").style.display= "none";
+    document.getElementById("champs2").style.display= "none";
+    document.getElementById("champs6").style.display= "none";
+    document.getElementById("epargnetype").style.display = "none"
+    document.getElementById("frais").style.display = "none"  
+
+}
+
+
+function requiere(params) {
+    if( params.value.trim() == ""){
+        document.getElementById("champs").style.display = "block"
+    }
+ 
 }
 
 function readForm(){
     var formdata = {};
-    formdata ['numCompte'] = document.getElementById("numCompte").value;
+    formdata ['idclient'] = document.getElementById("idclient").value;
+    formdata ['numCompte'] = document.getElementById("numCompte").value;idclient
     formdata ['numAgence'] = document.getElementById("numAgence").value;
-    formdata ['cleRib'] = document.getElementById("numCompte").value;
+    formdata ['cleRib'] = document.getElementById("numCompte").value+ " "+"cmp";
     formdata ['nom'] = document.getElementById("nom").value;
     formdata ['prenom'] = document.getElementById("prenom").value;
-    formdata ['typeclient'] = document.getElementById("typeclient").value;
+ //   formdata ['typeclient'] = document.getElementById("typeclient").value;
     formdata ['email'] = document.getElementById("email").value;
     formdata ['tel'] = document.getElementById("tel").value;
     formdata ['salaire'] = document.getElementById("salaire").value;
     formdata ['solde'] = document.getElementById("solde").value;
+    formdata ['dateO'] = document.getElementById("dateO").value;
+    formdata ['agio'] = document.getElementById("agio").value;
+    formdata ['epargne'] = document.getElementById("epargne").value;
+    formdata ['courant'] = document.getElementById("courant").value;
+    formdata ['bloque'] = document.getElementById("bloque").value;
 
     return formdata;
 }
 function insertCompte(data){
+    
     var tab   = document.getElementById("listecompte").getElementsByTagName("tbody")[0];
     var newCompte = tab.insertRow(tab.length);
     cell1 = newCompte.insertCell(0); 
@@ -30,19 +137,16 @@ function insertCompte(data){
     cell3 = newCompte.insertCell(2); 
     cell3.innerHTML = data.cleRib;
     cell4 = newCompte.insertCell(3); 
-    cell4.innerHTML = data.nom;
+    cell4.innerHTML = data.dateO;
     cell5 = newCompte.insertCell(4); 
-    cell5.innerHTML = data.prenom;
+    cell5.innerHTML = data.solde;
     cell6 = newCompte.insertCell(5); 
-    cell6.innerHTML = data.typeclient;
-    cell7 = newCompte.insertCell(6); 
-    cell7.innerHTML = data.email;
+    cell6.innerHTML = data.epargne +""+ data.courant+""+data.bloque ;
+    cell7 = newCompte.insertCell(6) ; 
+    cell7.innerHTML = data.agio;
     cell8 = newCompte.insertCell(7); 
-    cell8.innerHTML = data.tel;
-    cell9 = newCompte.insertCell(8); 
-    cell9.innerHTML = data.salaire;
-    cell10 = newCompte.insertCell(9); 
-    cell10.innerHTML = data.solde;
+    cell8.innerHTML = data.nom+" "+data.prenom+""+data.idclient
+
 
 
 }
@@ -50,14 +154,98 @@ function insertCompte(data){
 function masquerClient(){
         
         document.getElementById("masquer").style.display = "none";
-        document.getElementById("identifiant").style.display="block"
+        document.getElementById("identifiant").style.display="block";
+        document.getElementById("nouveau").checked = false;
 }
 //Pour afficher les infos client s'il est nouveau :
 
 function afficheElement(){
         
     document.getElementById("masquer").style.display = "block";
-    document.getElementById("identifiant").style.display="none"
+    document.getElementById("identifiant").style.display="none";
+    document.getElementById("ancien").checked = false;
 
 
+}
+
+ function annulChamp(){
+     document.getElementById("numCompte").value = "";
+     document.getElementById("numAgence").value = "";
+     document.getElementById("nom").value = "";
+     document.getElementById("prenom").value = "";
+     //document.getElementById("typeclient").value = "";
+     document.getElementById("email").value = "";
+     document.getElementById("tel").value = "";
+     document.getElementById("salaire").value = "";
+     document.getElementById("solde").value = "";
+     document.getElementById("epargne").value ="";
+     document.getElementById("courant").value ="";
+     document.getElementById("bloque").value="";
+     document.getElementById("simple").checked = false
+     document.getElementById("xeweul").checked = false
+     annulTout();
+
+}
+
+function salary(){
+        document.getElementById("employeur").style.display ="block"  
+        document.getElementById("nonsalaire").checked = false    
+        document.getElementById("entrepriseC").checked = false  
+}
+
+function entreprisee(){
+    document.getElementById("employeur").style.display ="none"    
+    document.getElementById("nonsalaire").checked = false   
+    document.getElementById("salarie").checked = false  
+}
+
+function non_salary(){
+    document.getElementById("employeur").style.display ="none"  
+    document.getElementById("entrepriseC").checked = false    
+    document.getElementById("salarie").checked = false           
+}
+function epargnee(){
+    document.getElementById("epargnetype").style.display = "block"
+    document.getElementById("frais").style.display = "block"  
+    document.getElementById("epargne").value ="epargne" 
+    document.getElementById("courant").value =""
+    document.getElementById("bloque").value = "" 
+
+    document.getElementById("courant").checked = false   
+    document.getElementById("bloque").checked = false      
+}
+function courante(){
+    document.getElementById("epargnetype").style.display = "none"  
+    document.getElementById("frais").style.display = "none"   
+    document.getElementById("courant").value ="courant" 
+    document.getElementById("epargne").value =""   
+    document.getElementById("bloque").value = "" 
+    document.getElementById("bloque").checked = false   
+    document.getElementById("epargne").checked = false  
+}
+
+function bloquee(){  
+    document.getElementById("epargnetype").style.display = "none"   
+    document.getElementById("frais").style.display = "block"   
+    document.getElementById("bloque").value = "bloque"   
+    document.getElementById("courant").value ="" 
+    document.getElementById("epargne").value ="" 
+    document.getElementById("courant").checked = false   
+    document.getElementById("epargne").checked = false        
+}
+
+function xeweule(){
+    document.getElementById("simple").checked = false      
+}
+function simplee(){
+    document.getElementById("xeweul").checked = false      
+}
+
+function deconnexion(){
+    var conf = confirm("voulez vous quitter ?")
+    if(conf == true){
+      window.location.replace("../index.html")
+    }else{
+      event.preventDefault();
+    }
 }
--- scrip.js
+++ scrip.js
@@ -1,16 +1,17 @@
+var login = document.getElementById("email");
+var pass = document.getElementById("password"); 
+var msg_error = document.getElementById("error");
+
 function testLogin (){
-    var login = document.getElementById("email");
-    var pass = document.getElementById("password"); 
-    var msg_error = document.getElementById("error");
     if(login.value == "mafatimegadiaga@gmail.com" && pass.value =="passer"){
         window.location.replace("./compte/compte.html");
        // alert("you are log in successfully")
     }else{
-        if(isNaN(login.value) && isNaN(pass.value)){
-            msg_error.textContent = "email ou password incorect";
+        if((login.value.trim() == "") || pass.value.trim() ==""){
+            msg_error.textContent = "veillez remplir tous les champs";
             msg_error.style.background = "red";
         }else{
-            msg_error.textContent = "veillez remplir tous les champs";
+            msg_error.textContent = "email ou password incorect";
             msg_error.style.background = "red";
         }
     }
